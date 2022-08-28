f73b38064418fe3c590612353a117875ff6fc23a
webDevAndres <macias.andres02@gmail.com>
2022-08-28 09:59:46 -0500

    week3

---
+++ week_3/macias_calculator.py
@@ -0,0 +1,19 @@
+def add(x,y):
+    return x + y
+
+def subtract(x,y):
+    return x - y
+
+def divide(x,y):
+    return x / y
+
+def multiply(x,y):
+    return x * y
+
+num1 = 10
+num2 = 2
+
+print(num1,"+", num2,"=",add(num1,num2))
+print(num1,"-", num2,"=",subtract(num1,num2))
+print(num1,"/", num2,"=",divide(num1,num2))
+print(num1,"*", num2,"=",multiply(num1,num2))
