2c841aaae07bd1f895537349ac9c2522a3b27391
webDevAndres <macias.andres02@gmail.com>
2022-08-28 21:15:53 -0500

    added comments

--- week_3/macias_calculator.py
+++ week_3/macias_calculator.py
@@ -1,3 +1,11 @@
+"""
+    Title: macias_calculator.py
+    Author: Andres Macias
+    Date: Aug 28 2022
+    Description: Week 3 exercise 3.3 Python Variables and Functions
+"""
+
+
 def add(x,y):
     return x + y
 
