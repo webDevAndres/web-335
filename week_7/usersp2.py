30e7ea58b10b46cc4a854a8fced90fb7c72aeb66
webDevAndres <macias.andres02@gmail.com>
2022-09-25 19:56:14 -0500

    queries for assignment 7.3 part 2

--- week_7/usersp2.py
+++ week_7/usersp2.py
@@ -1,10 +1,11 @@
-# Title: userspl.py
+# Title: usersp2.py
 # Author: Andres Macias
-# Date: 09/15/22
-# Description: Exercise 6.3 - Python with MongoDB, Part 1
+# Date: 09/25/22
+# Description: Exercise 7.3 - Python with MongoDB, Part 2
 
 # import MongoClient
 from pymongo import MongoClient
+import datetime
 
 # build connection string to connect to the client
 myclient = MongoClient(
@@ -12,12 +13,30 @@ myclient = MongoClient(
 
 db = myclient['web335DB']
 
-# Write the Python code to display all documents in the user’s collection.
-for user in db.users.find({}):
-    print(user)
+# Write the Python code to create a new user document.
+db.users.insert_one({
+    "firstName": "Joseph",
+    "lastName": "Haydn",
+    "employeeId": "1014",
+    "email": "jhaydn@me.com",
+    "dateCreated": datetime.datetime.now()
+})
 
-# Write the Python code to display a document where the employeeId is 1011.
-print(db.users.find_one({"employeeId": "1007"}))
 
-# Write the Python code to display a document where the lastName is Mozart.
-print(db.users.find_one({"lastName": "Mozart"}))
+# Write the Python code to display the newly created document.
+print(db.users.find_one({"employeeId": "1014"}))
+
+# Write the Python code to update the email address of the document you created in step 2.
+print(db.users.update_one(
+    {"email": "jhaydn@me.com"},
+    {"$set": {"email": "josephHaydn@me.com"}}
+))
+
+# Write the Python code to display the updated document.
+print(db.users.find_one({"email": "josephHaydn@me.com"}))
+
+# Write the Python code to delete the document you created in step 3.
+db.users.delete_one({"email": "josephHaydn@me.com"})
+
+# Write the Python code to prove the document was deleted.
+print(db.users.find_one({"email": "josephHaydn@me.com"}))
